ec772361b8287cc32d555f6bee337dbdbcf3f8c7
lazare2014k <lazaredavlasha@gmail.com>
2025-08-05 20:05:38 +0400

    commit

---
+++ level 03/classwork/cv.py
@@ -0,0 +1,19 @@
+ 
+#1)
+print(type(20))#integer
+print(type(12))#integer
+print(type('hello'))#string
+print(type('111'))#string
+print(type('1.5'))#string
+print(type(2.2))#float
+print('Group 74')#string
+print('Hello world')#string
+#2)
+name ="lazare"
+age =11
+print(name)
+print(age)
+name = "giorgi"
+age =16
+print (name)
+print (age)
---
+++ level 03/homework/hw.py
@@ -0,0 +1,6 @@
+x = 5
+y = 2.5
+print (x + y)
+#x-integer y-float
+print(type(y))
+print(type(x))
