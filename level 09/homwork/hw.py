48292431e96b2779ce2c4f4391dd388fff17419a
lazare2014k <lazaredavlasha@gmail.com>
2025-08-13 17:37:04 +0400

    commit

---
+++ level 09/homwork/hw.py
@@ -0,0 +1,14 @@
+#for loop ამარტივებს კოდების დაწერას და რამდენ რეინჯსაც დაწერ იმდენჯერ გაუშვებს კოდს
+for i in range(4):
+    print(i)
+
+for i in range(5):
+    print("სწავლა")
+for i in range(9):
+    print(i)
+
+for i in range(7):
+    print("hello")
+
+for i in range(3):
+    print("loop")
