39cf7c98998188625b8848c393d3ae910b547589
lazare2014k <lazaredavlasha@gmail.com>
2025-08-16 15:55:22 +0400

    commit

---
+++ level 011/homework/hw.py
@@ -0,0 +1,21 @@
+#while loopს ვიყენბეთ მაშინ  თუ წინასწარ არ იცი რამდენჯერ უნდა დაგჭირდეს იტერაცია 
+#for loopს ვიყენებთ მაშინ როცა ვიცით რამდენჯერ გამერდება კოდი,ხოლო while loopს ვიყენებთ მაშინ როცა არ ვიცით
+num = 0
+while num !=20:
+    print(num)
+    num= num + 1
+
+num1 = 0
+while num1 !=20:
+    print(num1)
+    num1= num1+ 2
+    
+num2= 10
+while num2 !=50:
+    print(num2)
+    num2= num2+ 5
+
+num3=70
+while num3 !=25:
+    print(num3)
+    num3= num3- 1
