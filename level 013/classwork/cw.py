171002ffd4a31fc51efbf8be47cf920154545e5d
lazare2014k <lazaredavlasha@gmail.com>
2025-08-18 16:05:36 +0400

    commit

---
+++ level 013/classwork/cw.py
@@ -0,0 +1,14 @@
+goa = 4
+goa1 = int(input ("enter number: "))
+while goa != goa1:
+    goa1 = int(input("enter number again: "))
+
+
+
+num = int(input("enter number: "))
+if num % 2==0:
+    print("even")
+else:
+    print("odd")
+
+
---
+++ level 013/homework/hw.py
@@ -0,0 +1,38 @@
+#while loop სანამ მისი პირობა არის true იქამდე აგრძელებს კოდის იტერაციას და ჩერდება მაშინ როცა ეს პრობა ახდება false
+num = 0
+while num !=4:
+    print(num)
+    num=num+1
+
+
+counter = 0
+while counter < 3:
+    print("გამარჯობა")
+    counter=counter+1
+
+num2 = 1
+while num2 != 5:
+    num2=num2+1
+    if num2 == 3:
+        print("საშუალო")
+
+
+
+
+num3 = 1
+while num3 != 5:
+    num3=num3+1
+    if num3 > 2:
+        print(num3)
+
+counter1 = 0
+while counter1 != 10:
+    counter1= counter1+1
+    print(counter1)
+    if counter1 == 4:
+        break
+
+
+
+
+
